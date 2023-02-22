409dc0bc2b3647428026db55bbb035b865290ac7
Ayander <124681608+Ayander@users.noreply.github.com>
2023-02-22 08:58:14 +0200

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   return (
    <main>
     <Header/>
-    Home Page
    </main>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import WebLogo from "../assets/Images/Wee Conquer gif.gif"
 
 const Header = () => {
     return (
-    <>
-        Header
-    </>
+        <header>
+            <nav>
+                <a href="index.html" class="nav-logo">
+                    <img src={WebLogo} alt="logo"/>
+                </a>
+                <ul class="nav-menu">
+                    <li class="menu-item">
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="About Us.html">About Us</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="externalWebsites.html">External websites</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="Contacts.html">Contacts</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="Donate.html">Donate</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
+export default Header
