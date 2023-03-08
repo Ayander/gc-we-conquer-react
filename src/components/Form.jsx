07f08fdc176708869058a2d1787aac3c4ba190a3
Ayander <124681608+Ayander@users.noreply.github.com>
2023-03-08 13:01:57 +0200

    Additions

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,57 +1,69 @@
 import React from "react"
 
 const Form = () => {
+const mySubmit = (evt) => {
+    evt.preventDefault()
+    const form = evt.target
 
+    let formData = new FormData(form)
+    console.log("Submitting", formData.getAll("Name"))
+}
     return (
 
         <>
             <body className="B1">
-                    <div class="container1">
-                        <div class="content">
-                            <div class="left-side">
-                                <div class="address details">
-                                    <i class="fa-solid fa-location-smile"></i>
-                                    <div class="topic">Address</div>
-                                    <div class="text-one">Centurion</div>
-                                    <div class="text-two">South Africa</div>
-                                </div>
-                                <div class="phone details">
-                                    <i class="fa-solid fa-phone"></i>
-                                    <div class="topic">Phone</div>
-                                    <div class="text-one">+27603093241</div>
-                                    <div class="text-two">+27679881880</div>
-                                </div>
-                                <div class="email details">
-                                    <i class="fa-solid fa-envelope"></i>
-                                    <div class="topic">Email</div>
-                                    <div class="text-one">ayandarsibanyoni@gmail.com</div>
-
-                                </div>
+                <div className="container1">
+                    <div className="content">
+                        <div className="left-side">
+                            <div className="address details">
+                                <i className="fa-solid fa-location-smile"></i>
+                                <div className="topic">Address</div>
+                                <div classNames="text-one">Centurion</div>
+                                <div className="text-two">South Africa</div>
                             </div>
-                            <div class="right-side">
-                                <div class="topic-text">Send us a message</div>
-                                <p>Tell us a bit about your case that way we know how to assist you</p>
-                                <form action="#">
-                                    <div class="input-box">
-                                        <input type="text" placeholder="Enter your name" />
-                                    </div>
-                                    <div class="input-box">
-                                        <input type="text" placeholder="Enter your email" />
-                                    </div>
-                                    <div class="input-box message-box">
-                                        <input type="text" placeholder="Massage" />
-                                    </div>
-                                    <div class="button">
-                                        <input type="button" value="Send Now" />
-                                    </div>
-                                </form>
+                            <div className="phone details">
+                                <i className="fa-solid fa-phone"></i>
+                                <div className="topic">Phone</div>
+                                <div classNames="text-one">+27603093241</div>
+                                <div className="text-two">+27679881880</div>
+                            </div>
+                            <div className="email details">
+                                <i className="fa-solid fa-envelope"></i>
+                                <div className="topic">Email</div>
+                                <div className="text-one">ayandarsibanyoni@gmail.com</div>
+
                             </div>
                         </div>
-                    </div>               
+
+                        <div className="right-side">
+                            <div className="topic-text">Send us a message</div>
+                            <p>Tell us a bit about your case that way we know how to assist you</p>
+
+                            <form onSubmit={mySubmit}>
+
+                                <div className="input-box">
+                                    <label htmlFor="Name">Name</label>
+                                    <input name="Name" type="text" placeholder="Enter your name" required />
+                                </div>
+                                <div className="input-box">
+                                <label htmlFor="Email">Email Address</label>
+                                    <input name="Email" type="text" placeholder="Enter your email" required />
+                                </div>
+                                <div className="input-box message-box">
+                                <label htmlFor="Massage">Massage</label>
+                                    <textarea name="Massage" id="" row="5" placeholder="Massage"></textarea>
+                                </div>
+                                <div className="button">
+                                    <input type="submit" value="Submit" />
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </body>
-            <script src="https://kit.fontawesome.com/75e10e2a6d.js" crossorigin="anonymous"></script>   
+            <script src="https://kit.fontawesome.com/75e10e2a6d.js" crossorigin="anonymous"></script>
         </>
-        
+
     )
 }
 export default Form
